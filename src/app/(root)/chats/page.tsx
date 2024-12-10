d5f56a2042552e474e8648b6d02bea0d42085bb4
YourGitHubUsername <aakashsethi101@gamil.com>
2024-12-10 17:52:31 +0530

    add currentId in the root of the chats

--- src/app/(root)/chats/page.tsx
+++ src/app/(root)/chats/page.tsx
@@ -3,11 +3,11 @@ import ChatList from "@/components/ChatList";
 import Contacts from "@/components/Contacts";
 import React from "react";
 
-export default function ChatPages() {
+export default function ChatPages({ params }: { params: any }) {
   return (
     <div className="main-container">
       <div className="w-1/3 max-lg:w-1/2 max-md:w-full">
-        <ChatList />
+        <ChatList currentChatId={params} />
       </div>
       <div className="w-2/3 max-lg:w-1/2 max-md:hidden">
         <Contacts />
